9e7d6ca552f1f5f29923d02ab98c2ba41605b432
joseph <langj6757@gmail.com>
2024-07-26 13:06:35 -0700

    Implement hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import NavBar from "@/components/navbar/navbar";
 import Banner from "../components/banner/banner";
+import Hero from "@/components/hero/hero";
 export default function Home() {
   return (
     <>
       <Banner />
       <NavBar />
+      <Hero />
     </>
   );
 }
---
+++ src/components/hero/hero.tsx
@@ -0,0 +1,75 @@
+import WhiteArrow from "../../../public/vector-white.svg";
+import cursorImage from "../../assets/images/cursor.png";
+import messageImage from "../../assets/images/message.png";
+import messageImage2 from "../../assets/images/message-2.png";
+import cursorImage2 from "../../assets/images/cursor-2.png";
+import Image from "next/image";
+const Hero = () => {
+  return (
+    <div className="bg-black text-white bg-[linear-gradient(to_bottom,#000,#200D42_34%,#4F21A1_65%,#A46EDB_82%)] relative overflow-hidden ">
+      <div className="absolute h-[375px] w-[750px] sm:w-[2100px] sm:h-[900px] rounded-[100%] bg-black left-1/2 -translate-x-1/2 border border-[#B48CDE] bg-[radial-gradient(closest-side,#000_90%,#9560EB)] top-[calc(100%-120px)]"></div>
+      <div className="flex flex-col gap-[32px] pt-[64.5px] pb-[89.5px] px-[27.5px] relative">
+        <div className="flex justify-center">
+          <a
+            href="#"
+            className="flex gap-2 items-center text-xs border border-white/30 rounded-lg px-2 py-1 w-max"
+          >
+            <span className="text-gray-400">Version 2.0 is here</span>
+            <span className="font-bold">Read more</span>
+            <div className="text-xl">
+              <WhiteArrow />
+            </div>
+          </a>
+        </div>
+        <div className="flex justify-center">
+          <div className="relative w-max">
+            <h1 className="text-[54px] lg:text-[200px] sm:text-[120px] tracking-tighter font-bold text-center lg:leading-[165px] sm:leading-[100px] leading-10">
+              One Task <br /> at a Time
+            </h1>
+            <Image
+              src={cursorImage2}
+              alt="cursor image"
+              height="200"
+              width="200"
+              className="absolute left-[-140px] top-[55%] hidden xl:block"
+            />
+            <Image
+              src={messageImage2}
+              alt="message image"
+              height="200"
+              width="200"
+              className="absolute right-[-170px] top-[32%] hidden xl:block"
+            />
+          </div>
+        </div>
+        <div className="flex justify-center">
+          <p className="text-center text-[22px] max-w-md">
+            Celebrate the joy of accomplishment with an app designed to track
+            your progress, motivate your efforts, and celebrate your successes.
+          </p>
+        </div>
+        <div className="flex justify-center">
+          <button className="bg-white px-5 py-2 rounded-md text-black font-medium">
+            Get for free
+          </button>
+        </div>
+      </div>
+      <Image
+        src={cursorImage}
+        alt="cursor image"
+        height="183"
+        width="183"
+        className="absolute left-[-10px] top-[22%] hidden sm:block xl:hidden"
+      />
+      <Image
+        src={messageImage}
+        alt="message image"
+        height="151"
+        width="151"
+        className="absolute right-[-10px] top-[30%] hidden sm:block xl:hidden"
+      />
+    </div>
+  );
+};
+
+export default Hero;
