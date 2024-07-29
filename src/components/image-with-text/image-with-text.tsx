61d4e78c0ed1fede1980e62cdea828e6701a66e6
joseph <langj6757@gmail.com>
2024-07-28 21:34:54 -0700

    Make image with text section responsive on all devices

--- src/components/image-with-text/image-with-text.tsx
+++ src/components/image-with-text/image-with-text.tsx
@@ -1,25 +1,52 @@
 import React from "react";
 import appImage from "../../assets/images/app.png";
+import mdAppImage from "../../assets/images/app-md.png";
+import xlAppImage from "../../assets/images/app-xl.png";
 import Image from "next/image";
 const ImageWithText = () => {
   return (
-    <div className="bg-[linear-gradient(to_bottom,#000,#46217E_34%,#05020A_100%)]">
+    <div className="bg-[linear-gradient(to_bottom,#000000,#4C248A_38%,#05020A_100%)]">
       <div className="flex flex-col text-white pt-[93px] px-[17.5px] pb-[60px] gap-[20px]">
-        <h2 className="text-[32px] font-semibold text-center">
+        <h2 className="text-[32px] md:text-[54px] font-semibold text-center">
           Intuitive Interface
         </h2>
-        <p className="text-center">
-          Celebrate the joy of accomplishment with an app designed to track your
-          progress, motivate your efforts, and celebrate your successes, one
-          task at a time.
-        </p>
+        <div className="flex justify-center">
+          <p className="text-center max-w-[535px] w-full">
+            Celebrate the joy of accomplishment with an app designed to track
+            your progress, motivate your efforts, and celebrate your successes,
+            one task at a time.
+          </p>
+        </div>
       </div>
-      <div className="flex justify-center w-full px-[17px] pb-[84px]">
-        <Image
-          src={appImage}
-          alt="app mockup"
-          className="w-full object-cover"
-        />
+      {/* Small App Image */}
+      <div className="flex justify-center">
+        <div className="flex w-[356px] h-[220px] md:hidden justify-center px-[17px] pb-[84px]">
+          <Image
+            src={appImage}
+            alt="app mockup"
+            className="w-full h-full object-cover"
+          />
+        </div>
+      </div>
+      {/* Medium App Image */}
+      <div className="flex justify-center">
+        <div className="justify-center w-[704px] h-[437px] px-[17px] pb-[84px] hidden md:flex xl:hidden">
+          <Image
+            src={mdAppImage}
+            alt="app mockup"
+            className="w-full h-full object-cover"
+          />
+        </div>
+      </div>
+      {/* Large App Image */}
+      <div className="flex w-full justify-center">
+        <div className="justify-center w-[1092px] h-[678px] px-[17px] pb-[84px] hidden xl:flex">
+          <Image
+            src={xlAppImage}
+            alt="app mockup"
+            className="w-full h-full object-cover"
+          />
+        </div>
       </div>
     </div>
   );
