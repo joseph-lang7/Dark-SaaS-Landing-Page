96bb30b070479ba9b55609a6b60edd9881735a00
Joseph Lang <langj6757@gmail.com>
2025-04-28 20:29:58 -0700

    update navbar component for improved layout on large screens

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -7,7 +7,7 @@ const NavBar = () => {
   return (
     <div>
       <div className="px-2 py-2">
-        <div className="flex items-center justify-between">
+        <div className="flex items-center justify-between lg:px-8">
           <div className="flex items-center">
             <Image src={Logo} alt="Sass logo" className=" object-cover" />
             <span className="text-white gap-2 hidden sm:inline-flex">
